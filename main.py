fa16fb72bdc97ca91db831b5f1cbc6b80453c7a7
silwal9 <silwal.prayas@gmail.com>
2023-05-11 19:11:36 +0545

    Show csv file in realtime in UI

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout
 from PyQt5.QtGui import QPixmap
 import sys
 import cv2
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread, QTimer      #
 import numpy as np
 from collections import Counter
+import csv  # import the csv module                                      #
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem              #
+
 strings=['']
 def tally(strings):
     # Create a dictionary to store the count of each string
@@ -21,11 +24,10 @@ class VideoThread(QThread):
 
     def run(self):
         # capture from web cam
-        cap = cv2.VideoCapture("vid1f.mp4")
+        cap = cv2.VideoCapture("vid1.mp4")
         i = 0
         while True:
             try:
-
                     ret, cv_img = cap.read()
                     cv_img,text = ocr.readtxt(cv_img)
                     strings.append(text)
@@ -43,16 +45,41 @@ class App(QWidget):
         self.setWindowTitle("Qt live label demo")
         self.disply_width = 640
         self.display_height = 480
+
+        # create the table widget                                       ##
+        self.tableWidget = QTableWidget(self)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(0)                              ##
+
+        # read data from CSV file and add to table widget
+        '''with open('data.csv', 'r') as file:
+            reader = csv.reader(file)
+            for i, row in enumerate(reader):
+                if i == 0:
+                    self.tableWidget.setColumnCount(len(row))
+                self.tableWidget.insertRow(i)
+                for j, val in enumerate(row):
+                    item = QTableWidgetItem(val)
+                    self.tableWidget.setItem(i, j, item)                ## '''
+
         # create the label that holds the image
         self.image_label = QLabel(self)
         self.image_label.resize(self.disply_width, self.display_height)
+
         # create a text label
         self.textLabel = QLabel(ttxt)
 
+        # create a text label for the video feed
+        self.video_label = QLabel('Video Feed', self)       #
+
+
         # create a vertical box layout and add the two labels
         vbox = QVBoxLayout()
+        vbox.addWidget(self.tableWidget)               #
+        vbox.addWidget(self.video_label)              #
         vbox.addWidget(self.image_label)
         vbox.addWidget(self.textLabel)
+
         # set the vbox layout as the widgets layout
         self.setLayout(vbox)
 
@@ -62,6 +89,25 @@ class App(QWidget):
         self.thread.change_pixmap_signal.connect(self.update_image)
         # start the thread
         self.thread.start()
+
+        # start a timer to periodically load data from CSV          ##
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.load_data)
+        self.timer.start(1000)                                      ##
+
+
+    def load_data(self):                                            ##
+        # read data from CSV file and add to table widget
+        with open('data.csv', 'r') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            self.tableWidget.setRowCount(len(rows))
+            self.tableWidget.setColumnCount(len(rows[0]))
+            for i, row in enumerate(rows):
+                for j, val in enumerate(row):
+                    item = QTableWidgetItem(val)
+                    self.tableWidget.setItem(i, j, item)            ##
+
 
     @pyqtSlot(np.ndarray)
     def update_image(self, cv_img):
